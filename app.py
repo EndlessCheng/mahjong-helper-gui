8a38c9f7abde607db3d1295f46214275810276fb
Endless <loli.con@qq.com>
2018-09-27 22:04:12 +0800

    修复显示 bug

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def tile_on_click_func(tile_index):
                     return
                 cnt[tile_index] -= 1
                 text_ctrl.SetValue(count_to_tiles(cnt))
-            send_tiles(event)
+            send_tiles_func(True)(event)
 
     return on_click
 
@@ -57,35 +57,49 @@ def reset(event):
     panel.Refresh()
 
 
-def send_tiles(event):
-    global is_interact_mode, cnt
-    is_interact_mode = True
-    if sum(cnt) < 14:
-        # 需要摸牌
-        for i, b in enumerate(grid_buttons):
-            if cnt[i] == 4:
-                b.Enabled = False
-    else:
-        # 需要切牌
-        for i, b in enumerate(grid_buttons):
-            if cnt[i] > 0:
-                b.Enabled = True
-    panel.Refresh()
+def send_tiles_func(need_interact):
+    def send_tiles(event):
+        global is_interact_mode, cnt
+        try:
+            # ~1s延迟
+            # TODO: waiting circle
+            resp = requests.post("http://localhost:12121/interact", json={
+                "tiles": text_ctrl.GetValue(),
+                "show_detail": sum(cnt) == 13,
+            })
+        except ConnectionRefusedError as e:
+            print("未连接上服务器", e)
+            return False
+        except Exception as e:
+            print(e)
+            return False
+        if resp.status_code != 200:
+            print(resp.text)
+            return False
 
-    # ~1s延迟
-    resp = requests.post("http://localhost:12121/interact", json={
-        "tiles": text_ctrl.GetValue(),
-    })
-    if resp.status_code != 200:
-        print(resp.text)
-        return False
-    return True
+        if need_interact:
+            is_interact_mode = True
+            if sum(cnt) < 14:
+                # 需要摸牌
+                for i, b in enumerate(grid_buttons):
+                    b.Enabled = cnt[i] < 4
+            else:
+                # 需要切牌
+                for i, b in enumerate(grid_buttons):
+                    b.Enabled = cnt[i] > 0
+            panel.Refresh()
+
+        return True
+
+    return send_tiles
 
 
 if __name__ == '__main__':
     app = wx.App()
     frame = wx.Frame(None, title='日麻辅助', size=(600, 480))
     panel = wx.Panel(frame)
+
+    # TODO: 撤销按钮
 
     text_ctrl = wx.TextCtrl(panel)
     reset_button = wx.Button(panel, label='重置')
@@ -105,12 +119,13 @@ if __name__ == '__main__':
         grid_sizer.Add(button, proportion=1, flag=wx.EXPAND, border=5)
         grid_buttons.append(button)
 
-    empty_label = wx.StaticText(panel, wx.ID_ANY, '')
-    grid_sizer.Add(empty_label, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
+    analysis_button = wx.Button(panel, label='分析')
+    analysis_button.Bind(wx.EVT_BUTTON, send_tiles_func(False))
+    grid_sizer.Add(analysis_button, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
 
-    send_button = wx.Button(panel, label='发送')
-    send_button.Bind(wx.EVT_BUTTON, send_tiles)
-    grid_sizer.Add(send_button, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
+    interact_button = wx.Button(panel, label='交互\n分析')
+    interact_button.Bind(wx.EVT_BUTTON, send_tiles_func(True))
+    grid_sizer.Add(interact_button, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
 
     vbox = wx.BoxSizer(wx.VERTICAL)
     vbox.Add(hbox, proportion=1, flag=wx.EXPAND | wx.ALL, border=5)
